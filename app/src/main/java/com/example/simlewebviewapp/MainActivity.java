b52bb58c705f3fc5a6bdef8ff397d42b87bd60a1
Volodymyr Shashkov <v.shashkov@morshynska.ua>
2022-12-17 13:44:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/simlewebviewapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.simlewebviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView wv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wv = (WebView) findViewById(R.id.wb_web_view);
+        wv.setWebViewClient(new WebViewClient());
+        wv.loadUrl("https://www.google.com.ua");
+        WebSettings webSettings = wv.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (wv.canGoBack()) {
+            wv.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
